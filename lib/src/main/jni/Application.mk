7a4ba7a5126128fcce4fcb66e6eb8b6c2aaeabae
Svetlozar Kostadinov <sevarbg@gmail.com>
2016-06-30 21:21:02 +0300

    changed APP_PLATFORM to 15

--- lib/src/main/jni/Application.mk
+++ lib/src/main/jni/Application.mk
@@ -2,5 +2,6 @@ APP_PROJECT_PATH := $(shell pwd)
 APP_ABI := armeabi-v7a armeabi x86
 NDK_MODULE_PATH=$(APP_PROJECT_PATH):$(APP_PROJECT_PATH)/src/main/jni/ndk-modules
 APP_STL := c++_static
-#APP_CPPFLAGS += -frtti -fexceptions
-APP_PLATFORM := 21
+# Warning: other than android-15 causes java.lang.UnsatisfiedLinkError: Cannot load library: reloc_library[1306]:   793 cannot locate '__ctype_get_mb_cur_max'...
+# when running on API 16
+APP_PLATFORM := android-15
