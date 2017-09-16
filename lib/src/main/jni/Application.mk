101fa65123a5700f16a58fd6efe4293c7bdca78d
Svetlozar Kostadinov <sevarbg@gmail.com>
2017-09-16 19:22:48 +0300

    Workaround for `missing mmap` error on NDK R15c

--- lib/src/main/jni/Application.mk
+++ lib/src/main/jni/Application.mk
@@ -4,3 +4,5 @@ APP_ABI := armeabi-v7a,arm64-v8a,x86,x86_64
 APP_PLATFORM := android-15
 NDK_TOOLCHAIN_VERSION := clang
 NDK_APP_LIBS_OUT=../jniLibs
+# Temp workaround for https://github.com/android-ndk/ndk/issues/332
+APP_DEPRECATED_HEADERS := true
