884e8a376aec691c1fdb79fa4029216401156a1f
Svetlozar Kostadinov <sevarbg@gmail.com>
2019-08-19 23:50:17 +0300

    Increase APP_PLATFORM to android-16

--- lib/src/main/jni/Application.mk
+++ lib/src/main/jni/Application.mk
@@ -1,7 +1,7 @@
 APP_STL := c++_shared
 APP_OPTIM := release
 APP_ABI := armeabi-v7a,arm64-v8a,x86,x86_64
-APP_PLATFORM := android-15
+APP_PLATFORM := android-16
 NDK_TOOLCHAIN_VERSION := clang
 NDK_APP_LIBS_OUT=../jniLibs
 # Temp workaround for https://github.com/android-ndk/ndk/issues/332
