dac0cc1652ebf16307e8d549f33d9c50081fd45d
sevar83 <sevarbg@gmail.com>
2019-09-04 22:55:01 +0300

    Refactor and clean up spatialite test

--- lib/src/androidTest/java/org/spatialite/SpatialiteTest.java
+++ lib/src/androidTest/java/org/spatialite/SpatialiteTest.java
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2007 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-// modified from original source see README at the top level of this project
-
 package org.spatialite;
 
 import android.database.Cursor;
@@ -31,7 +14,7 @@ import androidx.test.filters.SmallTest;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-@SuppressWarnings("ResultOfMethodCallIgnored")
+
 @RunWith(AndroidJUnit4.class)
 public class SpatialiteTest {
     
@@ -39,225 +22,158 @@ public class SpatialiteTest {
     private SQLiteDatabase mDatabase;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         mDatabase = SQLiteDatabase.openOrCreateDatabase(":memory:", null);
         assertNotNull(mDatabase);
         mDatabase.setVersion(CURRENT_DATABASE_VERSION);
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
         mDatabase.close();
     }
 
-    @SmallTest
-    @Test
-    public void testVersion_Spatialite() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT spatialite_version()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals("5.0.0-beta0", c.getString(0));
-        c.close();
+    private int getInt(String query) {
+        try (Cursor c = mDatabase.rawQuery(query, new Object[]{})) {
+            assertEquals(1, c.getCount());
+            c.moveToFirst();
+            return c.getInt(0);
+        }
+    }
+
+    private String getString(String query) {
+        try (Cursor c = mDatabase.rawQuery(query, new Object[]{})) {
+            assertEquals(1, c.getCount());
+            c.moveToFirst();
+            return c.getString(0);
+        }
     }
 
     @SmallTest
     @Test
-    public void testVersion_Proj4() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT proj4_version()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals("Rel. 4.9.3, 15 August 2016", c.getString(0));
-        c.close();
+    public void testVersion_Spatialite() {
+        assertEquals("5.0.0-beta0", getString("SELECT spatialite_version()"));
     }
 
     @SmallTest
     @Test
-    public void testVersion_GEOS() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT geos_version()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals("3.7.2-CAPI-1.11.2 b55d2125", c.getString(0));
-        c.close();
+    public void testVersion_Proj4() {
+        assertEquals("Rel. 4.9.3, 15 August 2016", getString("SELECT proj4_version()"));
     }
 
     @SmallTest
     @Test
-    public void testVersion_RtTopo() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT rttopo_version()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals("1.1.0-dev", c.getString(0));
-        c.close();
+    public void testVersion_GEOS() {
+        assertEquals("3.7.2-CAPI-1.11.2 b55d2125", getString("SELECT geos_version()"));
     }
 
     @SmallTest
     @Test
-    public void testHasIconv() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT HasIconv()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+    public void testHasIconv() {
+        assertEquals(1, getInt("SELECT HasIconv()"));
     }
 
     @SmallTest
     @Test
     public void testHasMathSQL() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT HasMathSQL()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+        assertEquals(1, getInt("SELECT HasMathSQL()"));
     }
 
     /*@SmallTest
     @Test
     public void testHasGeoCallbacks() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT HasGeoCallbacks()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+        assertEquals(1, getInt("SELECT HasGeoCallbacks()"));
     }*/
 
     @SmallTest
     @Test
     public void testHasProj() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT HasProj()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+        assertEquals(1, getInt("SELECT HasProj()"));
     }
 
     @SmallTest
     @Test
     public void testHasGeos() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT HasGeos()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+        assertEquals(1, getInt("SELECT HasGeos()"));
     }
 
     @SmallTest
     @Test
     public void testHasGeosAdvanced() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT HasGeosAdvanced()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+        assertEquals(1, getInt("SELECT HasGeosAdvanced()"));
     }
 
     /*@SmallTest
     @Test
     public void testHasGeosTrunk() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT HasGeosTrunk()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+        assertEquals(1, getInt("SELECT HasGeosTrunk()"));
     }*/
 
     /*@SmallTest
     @Test
     public void testHasLwGeom() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT HasLwGeom()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+        assertEquals(1, getInt("SELECT HasLwGeom()"));
     }*/
 
     @SmallTest
     @Test
     public void testHasLibXML2() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT HasLibXML2()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+        assertEquals(1, getInt("SELECT HasLibXML2()"));
     }
 
     @SmallTest
     @Test
     public void testHasEpsg() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT HasEpsg()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+        assertEquals(1, getInt("SELECT HasEpsg()"));
     }
 
     @SmallTest
     @Test
     public void testHasFreeXL() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT HasFreeXL()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+        assertEquals(1, getInt("SELECT HasFreeXL()"));
     }
 
     @SmallTest
     @Test
     public void testHasGeoPackage() throws Exception {
-        Cursor c = mDatabase.rawQuery("SELECT HasGeoPackage()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+        assertEquals(1, getInt("SELECT HasGeoPackage()"));
     }
 
     @SmallTest
     @Test
     public void testHasRtTopo() {
-        Cursor c = mDatabase.rawQuery("SELECT HasRtTopo()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+        assertEquals(1, getInt("SELECT HasRtTopo()"));
+    }
+
+    @SmallTest
+    @Test
+    public void testHasGCP() {
+        assertEquals(1, getInt("SELECT HasGCP()"));
     }
 
     @SmallTest
     @Test
     public void testHasTopology() {
-        Cursor c = mDatabase.rawQuery("SELECT HasTopology()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+        assertEquals(1, getInt("SELECT HasTopology()"));
     }
 
     @SmallTest
     @Test
     public void testHasKNN() {
-        Cursor c = mDatabase.rawQuery("SELECT HasKNN()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+        assertEquals(1, getInt("SELECT HasKNN()"));
     }
 
     @SmallTest
     @Test
     public void testHasRouting() {
-        Cursor c = mDatabase.rawQuery("SELECT HasRouting()", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals(1, c.getInt(0));
-        c.close();
+        assertEquals(1, getInt("SELECT HasRouting()"));
     }
 
     // sql_stmt_tests/asbinary1.testcase
     @SmallTest
     @Test
     public void testBinary1() {
-        Cursor c = mDatabase.rawQuery("SELECT Hex(AsBinary(GeomFromText(\"Point(1 2)\", 4326)))", new Object[] {});
-        assertEquals(1, c.getCount());
-        c.moveToFirst();
-        assertEquals("0101000000000000000000F03F0000000000000040", c.getString(0));
+        assertEquals("0101000000000000000000F03F0000000000000040", getString("SELECT Hex(AsBinary(GeomFromText(\"Point(1 2)\", 4326)))"));
     }
 
     @SmallTest
